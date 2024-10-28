70300281271ad5e594299e39bdfc9dee5713a664
andremendes <andregondek42@gmail.com>
2024-10-28 10:57:16 -0300

    utilização da array

--- src/Menu2.java
+++ src/Menu2.java
@@ -2,8 +2,14 @@ import java.util.Scanner;
 public class Menu2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int menu, matricula, horario = 800;
-        String nome = null, motivo;
+        int menu, horario = 800;
+        String AQV;
+        String[] aluno = new String[2];
+        String[] coordenacao = new String[2];
+        int id[] = new int[6];
+        String[] motivo = new String[2];
+        int matriculaAluno[] = new int[3];
+        int matriculaFuncionario[] = new int[3];
 
         do {
             System.out.println("Menu Principal" + "\n\t1- Cadastro" + "\n\t2- Consulta" + "\n\t3- Sair");
@@ -13,18 +19,25 @@ public class Menu2 {
 
             switch (menu){
                 case 1:
-                    System.out.println("Nome do aluno: ");
-                    nome = scanner.nextLine();
-                    System.out.println("Olá, " + nome + ". Escreva o motivo do seu atraso.");
-                    motivo = scanner.nextLine();
+                    for (int i = 0; i < aluno.length; i++) {
+                            System.out.println("Nome do aluno: ");
+                            aluno[i] = scanner.nextLine();
+                            System.out.println("Olá, " + aluno[i] + ". Escreva o motivo do seu atraso.");
+                            motivo[i] = scanner.nextLine();
+                    }
                     break;
                 case 2:
-                    if (nome != null) {
-                        System.out.println("Matrícula do aluno");
-                        matricula = scanner.nextInt();
-                        System.out.println("Olá, " + nome);
-                        break;
+                    System.out.println("\nNomes dos Alunos:");
+                    for (String nome : aluno) {
+                        System.out.println(nome);
                     }
+
+//                    if (nome != null) {
+//                        System.out.println("Matrícula do aluno");
+//                        matricula = scanner.nextInt();
+//                        System.out.println("Olá, " + nome);
+                        break;
+
                 default:
                     System.out.println("Opção Inválida. Informa uma opção existente!");
             }

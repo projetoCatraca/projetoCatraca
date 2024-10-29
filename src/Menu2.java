da352384696c09e98aeb6d79ec077fc6aa7418f2
Kauanne Oliveira <paulakauanne2411@gmail.com>
2024-10-29 16:04:48 -0300

    alteração em cadastro e consulta

--- src/Menu2.java
+++ src/Menu2.java
@@ -1,45 +1,86 @@
 import java.util.Scanner;
+
 public class Menu2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int menu, horario = 800;
-        String AQV;
-        String[] aluno = new String[2];
-        String[] coordenacao = new String[2];
-        int id[] = new int[6];
-        String[] motivo = new String[2];
-        int matriculaAluno[] = new int[3];
-        int matriculaFuncionario[] = new int[3];
+        int menu;
+        String[] alunos = new String[2];
+        String[] coordenadores = new String[2];
+        long[] matriculaAlunos = new long[2];
+        long[] matriculaFuncionarios = new long[3];
 
         do {
-            System.out.println("Menu Principal" + "\n\t1- Cadastro" + "\n\t2- Consulta" + "\n\t3- Sair");
+            // Menu Principal
+            System.out.println("Menu Principal \n\t1- Cadastro \n\t2- Consulta \n\t3- Sair");
             System.out.print("Escolha uma opção: ");
             menu = scanner.nextInt();
-            scanner.nextLine();
+            scanner.nextLine();   // Limpar o buffer
+            System.out.println(); // Espaço
 
             switch (menu){
                 case 1:
-                    for (int i = 0; i < aluno.length; i++) {
-                            System.out.println("Nome do aluno: ");
-                            aluno[i] = scanner.nextLine();
-                            System.out.println("Olá, " + aluno[i] + ". Escreva o motivo do seu atraso.");
-                            motivo[i] = scanner.nextLine();
+                    // Menu Usuário
+                    System.out.println("Você é: \n\t1- Aluno \n\t2- AQV \n\t3- Coordenador");
+                    System.out.print("Escolha uma opção: ");
+                    int escolha = scanner.nextInt();
+                    scanner.nextLine();
+                    System.out.println();
+
+                    if (escolha == 1) {
+                        // Alunos
+                        for (int i = 0; i < alunos.length; i++) {
+                            System.out.println("Nome do Aluno");
+                            alunos[i] = scanner.nextLine();
+
+                            System.out.println("Matricula");
+                            matriculaAlunos[i] = scanner.nextLong();
+                            scanner.nextLine();
+                        }
+                        System.out.println();
+                    } else if (escolha == 2) {
+                        // AQV
+                        System.out.println("Nome da(o) Analista de Qualidade de Vida (AQV)");
+                        String AQV = scanner.nextLine();
+
+                        System.out.println("Matricula");
+                        matriculaFuncionarios[0] = scanner.nextLong();
+                        scanner.nextLine();
+                        System.out.println();
+
+                    } else if (escolha == 3) {
+                        // Coordenadores
+                        for (int i = 0; i < coordenadores.length; i++) {
+                            System.out.println("Nome");
+                            coordenadores[i] = scanner.nextLine();
+
+                            System.out.println("Matricula");
+                            matriculaFuncionarios[i + 1] = scanner.nextLong();
+                            scanner.nextLine();
+                        }
+                        System.out.println();
+                    }
+                    else {
+                        System.out.println("Opção Inválida! Informa uma opção existente.");
+                        System.out.println();
                     }
                     break;
-                case 2:
-                    System.out.println("\nNomes dos Alunos:");
-                    for (String nome : aluno) {
-                        System.out.println(nome);
-                    }
 
-//                    if (nome != null) {
-//                        System.out.println("Matrícula do aluno");
-//                        matricula = scanner.nextInt();
-//                        System.out.println("Olá, " + nome);
-                        break;
+                case 2:
+                    System.out.println("\tAlunos Cadastrados");
+                    for (int i = 0; i < alunos.length; i++) {
+                        System.out.println("Nome: " + alunos[i]);
+                        System.out.println("Matrícula: " + matriculaAlunos[i]);
+                        System.out.println();
+                    }
+                    break;
+
+                case 3:
+                    System.out.println("Encerrando o programa...");
+                    break;
 
                 default:
-                    System.out.println("Opção Inválida. Informa uma opção existente!");
+                    System.out.println("Opção Inválida! Informa uma opção existente.");
+                    System.out.println();
             }
         } while (menu != 3);
 
